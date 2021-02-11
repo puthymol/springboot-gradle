2cd70aba34dae9a3399f900058af91073116477b
Puthy <puthymol@gmail.com>
2021-02-11 11:46:16 +0700

    Add feature AspectJ and ThreadPool

--- src/main/java/com/softvider/config/aspectj/AspectJConfig.java
+++ src/main/java/com/softvider/config/aspectj/AspectJConfig.java
@@ -2,7 +2,6 @@ package com.softvider.config.aspectj;
 
 import com.softvider.provider.aspectj.AppAspectJ;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(proxyTargetClass = true)
